1297d2463f69f0ce01220707fea59fdb5c844a2c
김민수 <min.su@daum.net>
2023-02-21 00:44:45 +0900

    [230220] feat : axios데이터통신,페이징기능

---
+++ reactnote.js
@@ -0,0 +1,126 @@
+"proxy": "http://localhost:8081",
+"proxy": "http://3.37.27.133:8081",
+
+{ 터미널 }
+npx create - react - app 프로젝트명    리액트 웹 앱 생성
+npm install react - router - dom    3페이지 구성을 위해 설치
+
+import styled from 'styled-components';
+let Box = styled.div`
+  padding : 20px;
+`;
+
+{ 사용 함수 }
+onclick
+Route
+BrowserRouter
+// URL과 UI를 새로고침 없이 동기화
+useState
+let [cnt, count] = useState(0)
+ex) <span onClick={() => { count(cnt + 1) }}></span>
+//함수형 컨포넌트 상태관리가능
+useEffect
+//앱을 실행했을때 처음 작동하기 위한
+useNavigate
+// 이벤트 발생시(ex 클릭) url을 조작
+Link
+//<Link to="/path"></Link>
+useParams
+//let { id } = useParams();
+//url 파라미터에 입력한값 = id로 출력
+
+// for(let [k,v] of Object.entries(item))
+
+probs
+//부모 자식 관계를 지정해줘서 state들을 상속받아서 사용가능 ex)probs.db.car_num[0]
+redux
+//컴포넌트들이 props없이 state공유 가능 외부라이브러리 개편함
+//  let name = useSelector((state) => { return state })
+// 뒤에 리턴뒤에 필요한것 쓸수있음 retern.abc state는 전부다
+{ 진행 과정 }
+리액트 웹 생성
+페이지 구성
+
+
+//Link
+
+
+import axios from 'axios'
+import { useState } from 'react'
+import { useParams } from 'react-router-dom'
+
+
+
+  로컬이 아닌 서버로부터 JSON 파일을 받기 위해서는 Axios, Fetch, XMLHttpRequest를 이용해야 합니다.
+
+  import
+
+let obj = {};
+console.log('object 생성 확인 =>', obj)
+let obglist = {
+  "key1": value1,
+  "key2": value2,
+  "key3": value3,
+}
+for (let k in obglist) {
+  console.log(k, objlist[k])
+  if (k === "filevalue") break
+}
+
+for (let [k, v] of Object.entries(objlist)) {
+  console.log(k, v);
+  if (k === 'filevalue') break
+}
+
+let lis = [];
+for (let [k, v] of Object.entries(mv)) {
+  lis.push(
+    <li key={k}>
+      <span className='mvLiK'>{k}</span>
+      <span className='mvLiV'>{v}</span>
+    </li>);
+}
+
+
+      <li>{db[0]?.x_gps}</li>
+      <li>{db[0] && db[0].x_gps}</li>
+
+dbname[0]?.filename
+1==1 && dbname[0].filename
+//백 프론트연결
+
+백
+
+프론트
+
+
+import axios 
+
+axios
+.get(/url)  / / url or port 불러오기
+  .then(response => { })  //stats에 담기 뭘할꺼냐
+  .catch((error) => { console.error(error) }) //안될때
+
+
+setProxy.js 파일을 생성
+  (필요한진 모르겠음)
+
+오브젝트 타입 읽기
+JSON.stringify("파일명")
+
+  
+
+
+  * 오답 노트
+첫 랜더링시 빈값이면 아예 다 리딩을안해버려서,
+  state 변수에 빈[] 배열로 해놓고 해결함.
+
+
+  // let test = [1, 2, 3, 4]
+  // let test2 = []
+
+  // let abc = test.map((a) => {
+  //   if (a > 2) {
+  //     test2.push(a);
+  //   }
+  // })
--- src/views/utilities/Page1.js
+++ src/views/utilities/Page1.js
@@ -8,37 +8,88 @@ const Page1 = () => {
   const [users, setUsers] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
+  const [page, setPage] = useState(1); // 현재 페이지 번호 상태값
+  const usersPerPage = 20; // 페이지당 보여줄 유저 수
+
+  const fetchUsers = async () => {
+    try {
+      // 요청이 시작 할 때에는 error 와 users 를 초기화하고
+      setError(null);
+      setUsers(null);
+      // loading 상태를 true 로 바꿉니다.
+      setLoading(true);
+      const response = await axios.get("/ui/page1");
+      setUsers(response.data); // 데이터는 response.data 안에 들어있습니다.
+    } catch (e) {
+      setError(e);
+    }
+    setLoading(false);
+  };
 
   useEffect(() => {
-    const fetchUsers = async () => {
-      try {
-        // 요청이 시작 할 때에는 error 와 users 를 초기화하고
-        setError(null);
-        setUsers(null);
-        // loading 상태를 true 로 바꿉니다.
-        setLoading(true);
-        const response = await axios
-          .get("/test");
-        setUsers(response.data); // 데이터는 response.data 안에 들어있습니다.
-      } catch (e) {
-        setError(e);
-      }
-      setLoading(false);
-    };
-
     fetchUsers();
   }, []);
-
-  console.log(users)
 
   if (loading) return <div>로딩중..</div>;
   if (error) return <div>에러가 발생했습니다</div>;
   if (!users) return null;
+
+
+  const totalPages = Math.ceil(users.length / usersPerPage); // 전체 페이지 수
+  // 현재 페이지에서 보여줄 유저 목록 계산
+  const startIndex = (page - 1) * usersPerPage;
+  const endIndex = startIndex + usersPerPage;
+  const currentPageUsers = users.slice(startIndex, endIndex);
+
+  // 페이지 번호 변경 함수
+  const handlePageChange = (pageNum) => {
+    setPage(pageNum);
+  };
+
+
+
   return (
     <PageContainer title="Page1 Page" description="this is Page1 page">
 
       <DashboardCard title="Page1">
-        <Typography>Page1</Typography>
+        <Typography>
+          <div>
+            <h1>User List</h1>
+            <table>
+              <thead>
+                <tr>
+                  <th>Num</th>
+                  <th>Age</th>
+                  <th>Name</th>
+                </tr>
+              </thead>
+              <tbody>
+                {currentPageUsers.map((user) => (
+                  <tr key={user.num}>
+                    <td>{user.num}</td>
+                    <td>{user.age}</td>
+                    <td>{user.name}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+            <div>
+              <button
+                disabled={page === 1}
+                onClick={() => handlePageChange(page - 1)}
+              >
+                이전 페이지
+              </button>
+              <button
+                disabled={page === totalPages}
+                onClick={() => handlePageChange(page + 1)}
+              >
+                다음 페이지
+              </button>
+            </div>
+            <div>현재 페이지: {page}</div>
+          </div>
+        </Typography>
       </DashboardCard>
     </PageContainer>
   );
